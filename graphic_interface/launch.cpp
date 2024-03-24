568a155cf869746e967861b88aae52225e61a46d
Daniel <officialpand@gmail.com>
2024-03-24 21:06:23 +0300

    fltk attempt

--- graphic_interface/launch.cpp
+++ graphic_interface/launch.cpp
@@ -1,21 +1,15 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
-#include <FL/Fl_Button.H>
-#include <FL/Fl_Input.H>
+#include <FL/Fl_Box.H>
 
-void button_callback(Fl_Widget* widget, void* data) {
-    Fl_Input* input = (Fl_Input*)data;
-    const char* input_text = input->value();
-}
-
-int main() {
-    Fl_Window* window = new Fl_Window(300, 200, "Пример приложения на FLTK");
-    Fl_Button* button = new Fl_Button(100, 100, 100, 30, "Нажми меня");
-    Fl_Input* input = new Fl_Input(100, 50, 100, 30, "Текст:");
-
-    button->callback(button_callback, (void*)input);
-
+int main(int argc, char **argv) {
+    Fl_Window *window = new Fl_Window(340, 180);
+    Fl_Box *box = new Fl_Box(20, 40, 300, 100, "Hello, World!");
+    box->box(FL_UP_BOX);
+    box->labelfont(FL_BOLD + FL_ITALIC);
+    box->labelsize(36);
+    box->labeltype(FL_SHADOW_LABEL);
     window->end();
-    window->show();
+    window->show(argc, argv);
     return Fl::run();
 }
