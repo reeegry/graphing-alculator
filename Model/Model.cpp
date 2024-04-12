c6942a36a288cc84a7ef447a99e98ab50e222bf4
DanielKorbelainen <officialpand@gmail.com>
2024-04-12 19:41:56 +0300

    model fixes

--- Model/Model.cpp
+++ Model/Model.cpp
@@ -1,17 +1,13 @@
 #include "Model.h"
 
+#include <algorithm>
+#include <cmath>
 #include <map>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <cmath>
 
 namespace {
     class Point {
-    private:
-        double x;
-        double y;
-
     public:
         Point(double xCoord, double yCoord) : x(xCoord), y(yCoord) {}
 
@@ -25,54 +21,55 @@ namespace {
             double dy = y - other.y;
             return std::sqrt(dx * dx + dy * dy);
         }
+    private:
+        double x;
+        double y;
     };
 
     class Function {
-    private:
-        std::string originalFunction;
-        std::map<int, std::string> functionDecomposition;
-        // o(log_n). Эффективное хранение элементов
+        public:
+            Function(const std::string &originalFunc) : originalFunction(originalFunc) {
+                std::transform(originalFunction.begin(), originalFunction.end(), originalFunction.begin(), ::tolower);
+            } // Для исправления на нижний регистр
 
-    public:
-        Function(const std::string &originalFunc) : originalFunction(originalFunc) {
-            std::transform(originalFunction.begin(), originalFunction.end(), originalFunction.begin(), ::tolower);
-        } // Для исправления на нижний регистр
-
-        // Разложение функции
-        void addDecomposition(int index, const std::string &decomposition) {
-            functionDecomposition[index] = decomposition;
-        }
-
-        // Исходная функция
-        std::string getOriginalFunction() const {
-            return originalFunction;
-        }
-
-        // Разложение функции по индексу
-        std::string getDecomposition(int index) const {
-            auto it = functionDecomposition.find(index);
-            if (it != functionDecomposition.end()) {
-                return it->second;
-            } else {
-                return "Invalid index";
+            // Разложение функции
+            void addDecomposition(int index, const std::string &decomposition) {
+                functionDecomposition[index] = decomposition;
             }
-        }
 
-        // Количество разложений
-        int getDecompositionCount() const {
-            return functionDecomposition.size();
-        }
+            // Исходная функция
+            std::string getOriginalFunction() const {
+                return originalFunction;
+            }
 
-        // Проверка на 1 переменную
-        bool hasSingleVariable() const {
-            return originalFunction.find("x") != std::string::npos;
-        }
+            // Разложение функции по индексу
+            std::string getDecomposition(int index) const {
+                auto it = functionDecomposition.find(index);
+                if (it != functionDecomposition.end()) {
+                    return it->second;
+                } else {
+                    return "Invalid index";
+                }
+            }
 
-        // Дополнение неполно заданной функции
-        std::string getDefaultFunction() const {
-            return (hasSingleVariable()) ? "y = " + originalFunction : originalFunction;
-        }
+            // Количество разложений
+            int getDecompositionCount() const {
+                return functionDecomposition.size();
+            }
+
+            // Проверка на 1 переменную
+            bool hasSingleVariable() const {
+                return originalFunction.find("x") != std::string::npos;
+            }
+
+            // Дополнение неполно заданной функции
+            std::string getDefaultFunction() const {
+                return (hasSingleVariable()) ? "y = " + originalFunction : originalFunction;
+            }
+        private:
+            std::string originalFunction;
+            std::map<int, std::string> functionDecomposition;
+            // o(log_n). Эффективное хранение элементов
+
     };
 }
-
-// Реализация
--- Model/Model.h
+++ Model/Model.h
@@ -1,12 +1,50 @@
-#ifndef GRAPHIC_INTERFACE_MODEL_H
-#define GRAPHIC_INTERFACE_MODEL_H
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <string>
+#include <vector>
+
+class Point {
+    public:
+        Point(double xCoord, double yCoord);
+
+        double getX() const;
+
+        double getY() const;
+
+        // Вычислить расстояние до другой точки
+        double distanceTo(const Point &other) const;
+
+    private:
+        double x;
+        double y;
+    };
+
+class Function {
+    public:
+        Function(const std::string &originalFunc);
+
+        // Добавить разложение функции
+        void addDecomposition(int index, const std::string &decomposition);
+
+        std::string getOriginalFunction() const;
+
+        // Получить разложение функции по индексу
+        std::string getDecomposition(int index) const;
+
+        // Получить количество разложений функции
+        int getDecompositionCount() const;
+
+        // Проверка на единственную переменную
+        bool hasSingleVariable() const;
 
 
-class Model {
+        // Получить дополненную функцию по умолчанию
+        std::string getDefaultFunction() const;
 
-};
-
-
-#endif //GRAPHIC_INTERFACE_MODEL_H
-
-// Описание
+    private:
+        std::string originalFunction; // исходная функция
+        std::map<int, std::string> functionDecomposition; // разложение функции
+    };
