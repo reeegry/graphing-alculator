337e90faf273b94847344036ba7e43de5f3fb591
Nikita <6u8654u4@gmail.com>
2024-05-27 17:33:28 +0300

    delete comms

--- view/mainwindow.cpp
+++ view/mainwindow.cpp
@@ -47,34 +47,18 @@ void MainWindow::on_lineEdit_editingFinished()
 void MainWindow::xAxisRangeChanged(const QCPRange &newRange, const QCPRange &oldRange)
 {
     Q_UNUSED(oldRange); // Используем это, чтобы избежать предупреждения о неиспользуемой переменной
-    //qDebug() << "X Axis Range Changed: " << newRange.lower << newRange.upper;
     if (!count) {
         return;
     }
-//    qDebug() << "Step: " << models[0]->funcParser->get_step() << '\n';
 
-    // if (models[0]->_points[0].size() > 0) {
-    //     double dist = std::abs(models[0]->_points[0][0] - models[0]->_points[0][models[0]->_points[0].size() - 1]); // if dist to small do nothing
-    //     if (newRange.lower > models[0]->_points[0][0] &&
-    //         newRange.upper < models[0]->_points[0][models[0]->_points[0].size() - 1]) {
 
-    //         return;
-    //     }
-    // }
-
-//    models[0]->funcParser->set_step(((newRange.upper - newRange.lower) / 1000));
-//    models[0]->getPoints(models[0]->getOriginalFunction(), newRange.lower, newRange.upper); // points in models[0]->_points
-//    if (!ui->widget->graphCount()) {
-//        ui->widget->addGraph();
-//    }
     models[0]->setStep(((newRange.upper - newRange.lower) / 1000));
     models[0]->getPoints(models[0]->getOriginalFunction(), newRange.lower, newRange.upper);
 
     if (!ui->widget->graphCount()) {
             ui->widget->addGraph();
     }
-//    qDebug() << "Size x: " << models[0]->_points[0].size() << '\n';
-//    qDebug() << "Size y: " << models[0]->_points[1].size() << '\n';
+
     ui->widget->graph(0)->setData(*models[0]->getX(), *models[0]->getY());
 
 }
